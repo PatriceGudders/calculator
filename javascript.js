4412b6a8c7c2d31834807538b70032155118833d
PatriceGudders <gudderspatrice@hotmail.com>
2025-03-19 15:51:30 +0100

    Create math functions
    Add, Subtract, Divide, Multiply

---
+++ javascript.js
@@ -0,0 +1,23 @@
+let numberOne;
+let numberTwo;
+let operator;
+
+function add(a, b) {
+    return a + b;
+}
+
+function subtract(a, b) {
+    return a - b;
+}
+
+function multiply(a, b) {
+    return a * b;
+}
+
+function divide(a, b) {
+    return a / b;
+}
+
+function operate(numberOne, numberTwo, operator) {
+    
+}
